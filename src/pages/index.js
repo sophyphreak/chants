8657f72231f098e76d6b92e88c5bb13d8c989986
Andrew Horn <arhorn@smcm.edu>
2021-03-13 04:23:18 -0600

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,289 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      1. <br />
+      No tar sands, <br />
+      No way, <br />
+      Not ever <br />
+      Not today
+    </p>
+    <p>
+      2. <br />
+      JP, you suck! <br />
+      You sold our future for a buck!
+    </p>
+    <p>
+      3. <br />
+      Blackrock, you suck! <br />
+      You sold our future for a buck!
+    </p>
+    <p>
+      4. <br />
+      No pipelines! <br />
+      No tar sands! <br />
+      No destruction of indigenous lands!
+    </p>
+    <p>
+      5. <br />
+      Hey CEO's up in the sky
+      <br />
+      <b>Listen up and hear our cry!</b> <br />
+      We don't run we block your pipe!
+      <br />
+      <b>You screw us and we multiply</b>
+    </p>
+    <p>
+      6. <br />
+      U-G-L-Y <br />
+      Corporate scurm you cheat you lie <br />
+      You UGLY, uh, uh, you UGLY
+    </p>
+    <p>
+      7. <br />
+      Tar sands kill! <br />
+      Pipelines spill!
+    </p>
+    <p>
+      8. <br />
+      Your pipeline dreams <br />
+      Just make me scream!
+    </p>
+    <p>
+      9. <br />
+      Hey hey, ho ho <br />
+      JP Morgan has got to go!
+    </p>
+    <p>
+      10. <br />
+      Hey hey, ho ho <br />
+      Blackrock has got to go
+    </p>
+    <p>
+      11. <br />
+      Pollution and exploitation <br />
+      Will not be solved by corporations. <br />
+      That's bullshit! Get off it! <br />
+      This planet's not for profit!
+    </p>
+    <p>
+      12. <br />
+      No exchanges no refunds, <br />
+      Environmental damage can't be undone!
+    </p>
+    <p>
+      13. <br />
+      Tar sands blockade! <br />
+      There's no stopping us <br />
+      Tar sands oil <br />
+      Dirty and dangerous!
+    </p>
+    <p>
+      14. <br />
+      People of the world unite! <br />
+      Let's stop the Line 3 pipe! <br />
+    </p>
+    <p>
+      15. <br />
+      Jobs at Line 3? No let's can it! <br />
+      There are no jobs on a dead planet!
+    </p>
+    <p>
+      16. <br />
+      Ain't no power like the power of the sun <br />
+      because the power of the sun don't stop!
+    </p>
+    <p>
+      17. <br />
+      When our planet's under attack, <br />
+      What do we do? <br />
+      Stand Up Fight Back!
+    </p>
+    <p>
+      18. <br />
+      No more coal, <br />
+      No more oil, <br />
+      Keep your carbon in the soil!
+    </p>
+    <p>
+      19. <br />
+      We are unstoppable, <br />A better world is possible!
+    </p>
+    <p>
+      20. <br />
+      Ain't no power like the power of people cause the power of people don't
+      stop!
+    </p>
+    <p>
+      21. <br />
+      That's bullshit! <br />
+      Get off it! <br />
+      This planet's not for profit!
+    </p>
+    <p>
+      22. <br />
+      When the land we love is under attack, what do we do? <br />
+      <b>Stand up, fight back!</b> <br />
+      When the air we breathe is under attack, what do we do? <br />
+      <b>Stand up, fight back!</b> <br />
+      When the water we drink is under attack, what do we do? <br />
+      <b>Stand up, fight back!</b> <br />
+      When the planet we need is under attack, what do we do? <br />
+      <b>Stand up, fight back!</b> <br />
+      What do we do? <br />
+      <b>Stand up, fight back!</b> <br />
+    </p>
+    <p>
+      23. <br />
+      People gonna rise like water <br />
+      We're gonna face this crisis now <br />
+      I hear the voice of my great granddaughter <br />
+      Saying 'climate justice now'!
+    </p>
+    <p>
+      24. <br />
+      This land is your land, this land is my land <br />
+      From California, to the New York Island <br />
+      From the Redwood Forest, to the Gulf Stream Waters <br />
+      This land was made for you and me
+    </p>
+    <p>
+      25. <br />
+      We shall overcome, <br />
+      We shall overcome, <br />
+      We shall overcome, some day. <br />
+      Oh, deep in my heart, <br />
+      I do believe <br />
+      We shall overcome, some day. <br />
+      <br />
+      We’ll walk hand in hand, <br />
+      We’ll walk hand in hand, <br />
+      We’ll walk hand in hand, some day. <br />
+      Oh, deep in my heart, <br />
+      I do believe <br />
+      We’ll walk hand in hand, some day. <br />
+      <br />
+      We are not afraid, <br />
+      We are not afraid, <br />
+      We are not afraid, today. <br />
+      Oh, deep in my heart, <br />
+      I do believe <br />
+      We are not afraid, some day. <br />
+      <br />
+      We shall overcome, <br />
+      We shall overcome, <br />
+      We shall overcome, some day. <br />
+      Oh, deep in my heart, <br />
+      I do believe <br />
+      We shall overcome, some day. <br />
+      <br />
+    </p>
+    <p>
+      26. <br />
+      Climate change is not a lie <br />
+      Do not let our planet die!
+    </p>
+    <p>
+      27. <br />
+      No pipelines, <br />
+      No tar sands! <br />
+      Stop destroying Native lands!
+    </p>
+    <p>
+      28. <br />
+      We. Are. Unstoppable. <br />
+      Another world is possible
+    </p>
+    <p>
+      29. <br />
+      I say "climate" you say "justice" we say "climate justice now!" <br />
+      Climate! <br />
+      <b>Justice!</b> <br />
+      Climate! <br />
+      <b>Justice!</b> <br />
+      (together) <b>Climate justice now!</b> <br />
+      (Same as previous but replace "justice" with "action")
+    </p>
+    <p>
+      30. <br />
+      (To the tune of <i>Amazing Grace</i>) <br />
+      Amazing shame, how lame the oil <br />
+      That wrecks our whole country <br />
+      I once used oil but now I try <br />
+      To save posterity <br />
+      Tis shame that lead to tell the truth <br />
+      Act as the truth is real <br />
+      How chained the future of our youth <br />
+      But truth will set them free <br />
+      Through government and corporate greed <br />
+      We have already won <br />
+      Rebel and bring them to their knees <br />
+      To save our only home <br />
+      Amazing shame, how lame the oil <br />
+      That wrecks our whole country <br />
+      I once used oil but now I try <br />
+      To save posterity
+    </p>
+    <p>
+      31. <br />
+      What do we want? <br />
+      <b>350 parts per million</b> <br />
+      When do we want it? <br />
+      <b>20 years ago</b>
+    </p>
+    <p>
+      32. <br />
+      Think about your legacy <br />
+      Fossil fuel or fossil free
+    </p>
+    <p>
+      33. <br />
+      The pipeline will fail <br />
+      Our planet's not for sale
+    </p>
+    <p>
+      34. <br />
+      No pipelines! No Tar Sands! <br />
+      <b>Stop destroying Native lands!</b>
+    </p>
+    <p>
+      35. <br />
+      No more corporate greed <br />
+      Renewables are what we need
+    </p>
+    <p>
+      36. <br />
+      Hey, hey, it's getting hot in here <br />
+      There's too much carbon in the atmosphere
+    </p>
+    <p>
+      37. <br />
+      Corporate greed we must fight <br />
+      Polluting Earth is not right
+    </p>
+    <p>
+      38. <br />
+      Invest in the future <br />
+      Not in pollution <br />
+      Put your money <br />
+      In the solution!
+    </p>
+    <p>
+      39. <br />
+      Chase Bank, you know how. <br />
+      Stop funding pipelines NOW!
+    </p>
+    <p>
+      40. <br />
+      Get up, get down. <br />
+      Leave fossil fuels in the ground! <br />
+      Get up, get down. <br />
+      There's climate justice in this town!
+    </p>
+    <p>
+      41. <br />
+      Hey hey, ho ho <br />
+      Climate change has got to go!
     </p>
   </Layout>
 )
